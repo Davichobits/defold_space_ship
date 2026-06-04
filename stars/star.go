b57331a12736f549c96d3206b663676a78db1aea
David Ruiz <davidruizdeveloper@gmail.com>
2026-06-04 09:34:01 -0500

    Star atlas

---
+++ stars/star.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Gem2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/Item/items.atlas\"\n"
+  "}\n"
+  ""
+}
