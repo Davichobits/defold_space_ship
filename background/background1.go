cdd32e482644acbc5d447f91d38f27a6029ff068
David Ruiz <davidruizdeveloper@gmail.com>
2026-06-16 08:33:57 -0500

    add background assets

---
+++ background/background1.go
@@ -0,0 +1,15 @@
+components {
+  id: "background"
+  component: "/background/background1.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/background/background.atlas\"\n"
+  "}\n"
+  ""
+}
---
+++ background/background2.go
@@ -0,0 +1,15 @@
+components {
+  id: "background2"
+  component: "/background/background2.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/background/background.atlas\"\n"
+  "}\n"
+  ""
+}
