4dd4117b0605b2cba8fccbd9a23c9b31d4d418eb
David Ruiz <davidruizdeveloper@gmail.com>
2026-06-17 08:35:38 -0500

    add camera config

---
+++ main/camera.go
@@ -0,0 +1,15 @@
+components {
+  id: "camera1"
+  component: "/main/camera.script"
+}
+embedded_components {
+  id: "camera"
+  type: "camera"
+  data: "aspect_ratio: 1.0\n"
+  "fov: 0.7854\n"
+  "near_z: 0.1\n"
+  "far_z: 1000.0\n"
+  "orthographic_projection: 1\n"
+  "orthographic_mode: ORTHO_MODE_AUTO_FIT\n"
+  ""
+}
